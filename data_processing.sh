2b17c08df5af7c6ea056d8dd50810de0dc7f39fa
micacer22 <154255937+micacer22@users.noreply.github.com>
2025-01-31 14:39:12 +0100

    Create data_processing.sh

---
+++ data_processing.sh
@@ -0,0 +1,14 @@
+## Data Preprocessing script ##
+
+# Analisis de calidad con Fastqc antes de trimmear
+nohup fastqc -t 8 *.fastq.gz &
+ 
+# Trim_galore
+ nohup trim_galore --length 10 --max_length 40 *.fastq.gz &
+
+# Fastqc de los archivos trimeados
+ for file in *_trimmed.fq.gz; 
+ do     fastqc "$file" -o ./fastqc_trimming_results/; done
+
+# Multiqc de los archivos fastqc trimeados
+multiqc . 
