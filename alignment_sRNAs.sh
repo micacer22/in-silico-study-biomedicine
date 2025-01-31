348da1c16a22ccd0b2396d6df8de88eb44358188
micacer22 <154255937+micacer22@users.noreply.github.com>
2025-01-31 14:50:44 +0100

    Update alignment_sRNAs.sh

--- alignment_sRNAs.sh
+++ alignment_sRNAs.sh
@@ -4,3 +4,15 @@ nohup ShortStack \
   --readfile /home/cacermi/miriam/2024/InSilico/proyecto_epigenetica/Data/SRR/combined/sra_files/*_trimmed.fq.gz \
   --outdir /home/cacermi/miriam/2024/InSilico/proyecto_epigenetica/Data/SRR/combined_output \
   --bowtie_cores 8 > combined_analysis.log 2>&1 &
+
+# Separar el archivo merged de mapeo en los distintos archivos .bam separados
+for rg in $(samtools view -H merged_alignments.bam | grep '@RG' | cut -f2 | sed 's/ID://'); do
+    echo "Procesando $rg..."
+    samtools view -b -r $rg merged_alignments.bam > ${rg}.bam
+done
+
+# Ordenar archivos .bam separados
+nohup bash -c 'for bamfile in SRR*_trimmed.bam; do samtools sort $bamfile -o ${bamfile%.bam}_sorted.bam; done' &
+
+# Indexar archivos _soted.bam
+nohup bash -c 'for file in *_trimmed_sorted.bam; do samtools index "$file"; done' > index.log 2>&1 &
